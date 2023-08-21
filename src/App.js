5ebfff0f25e39e9975341987262aeff8c68a25fc
Ramkumar kushwah <kushwahramkumar2003@gmail.com>
2023-08-21 23:46:38 +0530

    router-dom added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.render(
-  <h1 className="text-orange-600">Hello world!</h1>,
-  document.getElementById("root")
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "team",
+        element: <Team />,
+        loader: teamLoader,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
